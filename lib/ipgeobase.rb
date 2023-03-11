dc2b1345fc89f154192d824d108b4a82d571a193
Dasha Tikhomirova <Tikhomirova.DG@tversu.ru>
2023-03-11 17:18:24 +0300

    Service address moved to separated var, comment before module Ipgeobase removed

--- lib/ipgeobase.rb
+++ lib/ipgeobase.rb
@@ -4,10 +4,11 @@ require_relative 'ipgeobase/version'
 require_relative 'ipgeobase/geodata'
 require 'net/http'
 
-# main
 module Ipgeobase
+  SERVICE_URL = 'http://ip-api.com/xml'
+
   def self.lookup(ip_address)
-    uri = URI("http://ip-api.com/xml/#{ip_address}")
+    uri = URI("#{SERVICE_URL}/#{ip_address}")
     response = Net::HTTP.get_response(uri)
     return Geodata.parse(response.body, single: true) if response.is_a?(Net::HTTPSuccess)
 
